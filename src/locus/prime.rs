686d936342da4465fc35112792d33f56828983ac
Matt Lawlor <matt.a.lawlor@gmail.com>
2019-04-04 12:55:06 -0400

    added 5 finder

--- src/locus/prime.rs
+++ src/locus/prime.rs
@@ -5,22 +5,39 @@ use bio_types::strand::*;
 
 // define EitherEnd for all types implementing loc
 pub trait EitherEnd : Loc {
-    fn fiveprime(&self) -> Pos<Self::RefID, Self::Strand>;
-    //fn threeprime(self) -> Self;
+    fn fiveprime(&self) -> Pos<Self::RefID, Self::Strand>
+    where
+        Self::RefID: Copy,
+        Self::Strand: Into<ReqStrand> + Copy;
+    fn threeprime(&self) -> Pos<Self::RefID, Self::Strand>
+    where
+        Self::RefID: Copy,
+        Self::Strand: Into<ReqStrand> + Copy;
 }
 
-/*
+
 impl<T: Loc> EitherEnd for T {
     fn fiveprime(&self) -> Pos<Self::RefID, Self::Strand>
-    //where
-    //    Self::RefID: Clone,
-    //    Self::Strand: Into<ReqStrand> + Copy
+    where
+        Self::RefID: Copy,
+        Self::Strand: Into<ReqStrand> + Copy,
     {
         match self.strand().into() {
-            ReqStrand::Forward => self.first_pos(),
             ReqStrand::Reverse => self.last_pos(),
-        }
+            _ => self.first_pos(),
 
+        }
+    }
+
+    fn threeprime(&self) -> Pos<Self::RefID, Self::Strand>
+    where
+        Self::RefID: Copy,
+        Self::Strand: Into<ReqStrand> + Copy,
+    {
+        match self.strand().into() {
+            ReqStrand::Reverse => self.first_pos(),
+            _ => self.last_pos(),
+
+        }
     }
 }
-*/
