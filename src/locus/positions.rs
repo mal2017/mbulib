f2b980729b179ee8e0bd316ab3d95758e032ccb4
Matt Lawlor <matt.a.lawlor@gmail.com>
2019-04-13 11:59:25 -0400

    working pos iterator, need to add support for Non-reqstrand and stranded stuff, currently just left-to-right

--- src/locus/positions.rs
+++ src/locus/positions.rs
@@ -15,58 +15,31 @@ use rust_htslib::bam::Read;
 use std::cmp::{min, max};
 use crate::locus::shift::Shift;
 
-// #[derive(Debug)]
-// pub struct Positions<'a, R: 'a + Loc>
-//     where
-//         R::RefID: Clone,
-//         R::Strand: Copy + Into<strand::Strand>,
-//     {
-//     region: &'a R,
-//     idx: isize,
-//     pos: &'a Pos<String, strand::Strand>,
-// }
-//
-// impl<'a, L: Loc> Iterator for Positions<'a, L>
-//     where
-//         L::RefID: Clone,
-//         L::Strand: Copy + Into<strand::Strand>,
-//     {
-//     type Item = Result<Pos<String, strand::Strand>, &'static str>;
-//
-//     fn next(&mut self) -> Option<Result<Pos<String, strand::Strand>, &'static str>>
-//         {
-//         let sp = self.region.start();
-//
-//         let len = self.length()s;
-//
-//         //let p = self.region.first_pos();
-//         self.idx += 1;
-//         Some(Ok(pos))
-//
-//     }
-// }
-
 #[derive(Debug)]
 pub struct Positions<'a> {
     region: &'a Contig<String,strand::ReqStrand>,
     idx: isize,
 }
 
+// This returns and Options<Result> for now, in case I want to implement more error checking later
 impl<'a> Iterator for Positions<'a> {
     type Item = Result<Pos<String, strand::Strand>, &'static str>;
     fn next(&mut self) -> Option<Result<Pos<String, strand::Strand>, &'static str>>
         {
-        let sp = self.region.start();
-        let w = self.region.length();
-
-        let strand: strand::Strand = self.region.strand().into();
-
-        let refid: String = self.region.refid().to_string();
-
-        let pos = Pos::new(refid, self.idx, strand);
-        self.idx += 1;
-        Some(Ok(pos))
-
+            let w = self.region.length();
+        match (self.idx as usize) < w {
+            true => {
+                let sp = self.region.start();
+                let strand: strand::Strand = self.region.strand().into();
+                let refid: String = self.region.refid().to_string();
+                let pos = Pos::new(refid, sp + self.idx, strand);
+                self.idx += 1;
+                Some(Ok(pos))
+            },
+            false => {
+                None
+            }
+        }
     }
 }
 
@@ -100,14 +73,14 @@ mod tests {
         let mut bam = bam::Reader::from_path(bampath).unwrap();
         let hdrv = bam.header();
         let tidmap = ScaffoldDict::from_header_view(&hdrv);
-        let truth = "chr1:564476-564511(+)";
         let res = bam
             .records()
             .map(|a| a.unwrap())
-            .take(1)
+            .take(2)
             .map(|a| Contig::from_read(&a, false, &tidmap));
 
         for i in res.into_iter() {
+            println!("\nREAD: {:?}",i);
             i.positions().for_each(|a| println!("{:?}",a));
         }
     }
